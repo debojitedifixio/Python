951ab735dc97bb083979797e6908c8cabbf6441d
debojit-cr7 <debojitsadhukhan1994@gmail.com>
2023-02-06 12:53:07 +0530

    Functions with Outputs

---
+++ Function_with_outputs/calculator.py
@@ -0,0 +1,62 @@
+# Calculator
+# Add
+from art import logo
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+# substract
+def sub(n1, n2):
+    return n1 - n2
+
+
+# Multiply
+def mul(n1, n2):
+    return n1 * n2
+
+
+# divide
+def div(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add,
+              "-": sub,
+              "*": mul,
+              "/": div
+              }
+
+print(logo)
+
+
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    flag = True
+    while flag:
+        for symbols in operations:
+            print(symbols)
+        operations_symbol = input("Select a operation symbol: ")
+
+        num2 = float(input("Enter the next number: "))
+
+        answer = operations[operations_symbol](num1, num2)
+
+        print(f"{num1} {operations_symbol} {num2} = {answer}")
+
+        choice = input(
+            f"If you want to continue with previous result {answer} press 'Y' else press 'N' to start a new "
+            f"calculation and type any to exit. ").lower()
+
+        if choice == 'y':
+            num1 = answer
+        elif choice == 'n':
+            flag = False
+            calculator()
+        else:
+            flag = False
+            print(f"Final Result : {answer}")
+
+
+calculator()
---
+++ Function_with_outputs/new1.py
@@ -0,0 +1,10 @@
+# create a function
+
+def format_name(first_name, last_name):
+    formated_f_name = first_name.title()
+    formated_l_name = last_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name("dEboJit", "SaDhuKhAN"))
+
