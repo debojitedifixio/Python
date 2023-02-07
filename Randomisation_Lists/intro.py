906e72d02d3362cc76adfb6a7c244fd8d35790a0
debojit-cr7 <debojitsadhukhan1994@gmail.com>
2023-02-07 10:09:40 +0530

    BlackJack

--- Randomisation_Lists/intro.py
+++ Randomisation_Lists/intro.py
@@ -1,18 +1,17 @@
-#its basically used to generate random numbers
+# its basically used to generate random numbers
 
 import random as rd
 
-#generate random integer numbers using range 0-10
-random_integer =  rd.randint(0,10)
+# generate random integer numbers using range 0-10
+random_integer = rd.randint(0, 10)
 print(random_integer)
 
-#generates a floating random from 0.0000-0.9999, 
+# generates a floating random from 0.0000-0.9999,
 # to generate the decimal number we can use multiplication
- 
+
 
 randoam_float = rd.random() * 5
 print(randoam_float)
-
 
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 fruits.count('apple')
@@ -34,29 +33,28 @@ print(fruits)
 
 fruits.pop()
 
-#print the list of Squred items
+# print the list of Squred items
 
 squares = []
 for x in range(10):
-    squares.append(x**2)
+    squares.append(x ** 2)
 
 print(squares)
 
+# nested List
 
-#nested List
-
-a = [1,2,3]
-b = [4,5,6]
-c=[a,b]
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [a, b]
 
 print(c)
 
-#nested loop another one
+# nested loop another one
 
 fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
 vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
- 
+
 dirty_dozen = [fruits, vegetables]
- 
-print(dirty_dozen[1][2])  #prints 2nd list 3rd item
+
+print(dirty_dozen[1][2])  # prints 2nd list 3rd item
 print(dirty_dozen[1][3])
